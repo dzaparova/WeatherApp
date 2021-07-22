6772bedb583b044ffa3c7369eb9d2a8ac669aa56
MyTwoFRagment <dzaparovap@gmail.com>
2021-07-22 20:08:54 +0600

    corrected in gradle

--- app/src/main/java/kg/tutorialapp/weather/fcm/MyFirebaseService.kt
+++ app/src/main/java/kg/tutorialapp/weather/fcm/MyFirebaseService.kt
@@ -20,37 +20,37 @@ import java.util.stream.Stream.builder
 
 
 class MyFirebaseService:FirebaseMessagingService() {
-//    override fun onNewToken(p0: String) {
-//        super.onNewToken(p0)
-//    }
-//
-//    @RequiresApi(Build.VERSION_CODES.O)
-//    override fun onMessageReceived(remoteMessage: RemoteMessage) {
-//        val intent = Intent(this, MainActivity::class.java).apply {
-//            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-//            putExtra(String::class.java.canonicalName, remoteMessage.data["body"])
-//        }
-//        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
-//
-//        val channelId = "myChannel"
-//
-//        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-//
-//        val channel =
-//            NotificationChannel(channelId, "default", NotificationManager.IMPORTANCE_DEFAULT)
-//        notificationManager.createNotificationChannel(channel)
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            putExtra(String::class.java.canonicalName, remoteMessage.data["body"])
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+
+        val channelId = "myChannel"
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        val channel =
+            NotificationChannel(channelId, "default", NotificationManager.IMPORTANCE_DEFAULT)
+        notificationManager.createNotificationChannel(channel)
 
 
-//       val notification = Notification.Builder(this, channelId)
-//           .setContentTitle(remoteMessage.notification?.title)
-//           .setContentText(remoteMessage.notification?.body)
-//           .setContentIntent(pendingIntent)
-//           .setSmallIcon(R.drawable.ic_water_drop)
-//           .setAutoCancel(true)
-//           .build()
-//
-//       notificationManager.notify(System.currentTimeMillis().toInt(), notification)
-//    }
+       val notification = Notification.Builder(this, channelId)
+           .setContentTitle(remoteMessage.notification?.title)
+           .setContentText(remoteMessage.notification?.body)
+           .setContentIntent(pendingIntent)
+           .setSmallIcon(R.drawable.ic_water_drop)
+           .setAutoCancel(true)
+           .build()
+
+       notificationManager.notify(System.currentTimeMillis().toInt(), notification)
+    }
 
 
 }
